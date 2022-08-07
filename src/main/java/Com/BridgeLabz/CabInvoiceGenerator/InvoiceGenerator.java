e1ad1d3781be64aee059862372db9a3a9ce24cec
Akhil-Choudhari <akhil.choudhari.ac@gmail.com>
2022-08-07 20:08:09 +0530

    ADDED: Calculate the total fare of the journey

--- src/main/java/Com/BridgeLabz/CabInvoiceGenerator/InvoiceGenerator.java
+++ src/main/java/Com/BridgeLabz/CabInvoiceGenerator/InvoiceGenerator.java
@@ -5,13 +5,11 @@ public class InvoiceGenerator {
     private static final int COST_PER_TIME = 1;
     private static final double COST_PER_KM = 10;
     private static final double MINIMUM_FARE = 5;
-
     /**
      * create method to calculate total fare as per distance and time
-     *
      * @param distance -distance of per km cost is 10 rs
      * @param time     -per minute cost is 1rs
-     * @return total fare -total fare to be calulated
+     * @return total fare -total fare to be calculate
      */
     public double calculateFare(double distance, int time) {
         return Math.max(MINIMUM_FARE, distance * COST_PER_KM + time * COST_PER_TIME);
